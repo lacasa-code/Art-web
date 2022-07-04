415b3b5cdf3c101aba8b1e953087c2336fc1fc46
نورهان طاهر <norhan@lacasacode.come>
2022-07-05 00:35:08 +0200

    update

--- resources/views/frontend/inc/nav.blade.php
+++ resources/views/frontend/inc/nav.blade.php
@@ -164,7 +164,7 @@
                             <a href="{{ route('user.registration') }}" class="text-reset d-inline-block opacity-60 py-2" style="color: #FFFFFF;"> {{ translate('Registration')}}</a>
                         </li>
                         <li class="list-inline-item" style="color: #FFFFFF;">
-                            <a href="{{ route('shops.create') }}" class="btn btn-primary btn-sm shadow-md" style="color: #FFFFFF;">
+                            <a href="{{ route('shops.create') }}"  style="color: #FFFFFF;">
                                {{ translate('Be a Seller') }}
                             </a>
                         </li>
