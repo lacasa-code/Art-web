2c26e10fdfa37bb491b9ca2588e2537689ff0df1
نورهان طاهر <norhan@lacasacode.come>
2022-07-05 15:20:49 +0200

    update

--- resources/views/frontend/inc/footer.blade.php
+++ resources/views/frontend/inc/footer.blade.php
@@ -7,19 +7,19 @@
                     <h4 class="h6">{{ translate('Terms & conditions') }}</h4>
                 </a>
             </div>
-            <div class="col-lg-3 col-md-6">
+            <div class="col-lg-3 col-md-6" style="background-color: #FFFFFF;">
                 <a class="text-reset border-left text-center p-4 d-block" href="{{ route('returnpolicy') }}">
                     <i class="la la-mail-reply la-3x text-primary mb-2"></i>
                     <h4 class="h6">{{ translate('Return Policy') }}</h4>
                 </a>
             </div>
-            <div class="col-lg-3 col-md-6">
+            <div class="col-lg-3 col-md-6" style="background-color: #FFFFFF;">
                 <a class="text-reset border-left text-center p-4 d-block" href="{{ route('supportpolicy') }}">
                     <i class="la la-support la-3x text-primary mb-2"></i>
                     <h4 class="h6">{{ translate('Support Policy') }}</h4>
                 </a>
             </div>
-            <div class="col-lg-3 col-md-6">
+            <div class="col-lg-3 col-md-6" style="background-color: #FFFFFF;">
                 <a class="text-reset border-left border-right text-center p-4 d-block" href="{{ route('privacypolicy') }}">
                     <i class="las la-exclamation-circle la-3x text-primary mb-2"></i>
                     <h4 class="h6">{{ translate('Privacy Policy') }}</h4>
