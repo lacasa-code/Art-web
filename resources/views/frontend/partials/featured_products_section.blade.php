41a9fd28133ea8757f4e0417ff7652b6b654ca85
نورهان طاهر <norhan@lacasacode.come>
2022-07-05 15:49:05 +0200

    update

--- resources/views/frontend/partials/featured_products_section.blade.php
+++ resources/views/frontend/partials/featured_products_section.blade.php
@@ -6,8 +6,8 @@
 
 @if (count($featured_products) > 0)
     <section class="mb-4">
-        <div class="container" style="background-color: #1D4C58;">
-            <div class="px-2 py-4 px-md-4 py-md-3 bg-white shadow-sm rounded">
+        <div class="container" >
+            <div class="px-2 py-4 px-md-4 py-md-3 bg-white shadow-sm rounded" style="background-color: #1D4C58;">
                 <div class="d-flex mb-3 align-items-baseline border-bottom">
                     <h3 class="h5 fw-700 mb-0">
                         <span class="border-bottom border-primary border-width-2 pb-3 d-inline-block">{{ translate('Featured Products') }}</span>
