129c3acca454d80057371c6650de0d65e011b836
نورهان طاهر <norhan@lacasacode.come>
2022-07-05 11:03:16 +0200

    update

--- resources/views/frontend/partials/wishlist.blade.php
+++ resources/views/frontend/partials/wishlist.blade.php
@@ -2,9 +2,9 @@
     <i class="la la-heart-o la-2x opacity-80"></i>
     <span class="flex-grow-1 ml-1">
         @if(Auth::check())
-            <span class="badge badge-light badge-inline badge-pill">{{ count(Auth::user()->wishlists)}}</span>
+            <span class="badge badge-primary badge-inline badge-pill">{{ count(Auth::user()->wishlists)}}</span>
         @else
-            <span class="badge badge-light badge-inline badge-pill">0</span>
+            <span class="badge badge-primary badge-inline badge-pill">0</span>
         @endif
         <span class="nav-box-text d-none d-xl-block opacity-70" style="color: #FFFFFF;">{{translate('Wishlist')}}</span>
     </span>
