77e6bcf191cdec92d25f1e45fd2b7965a56c119e
نورهان طاهر <norhan@lacasacode.come>
2022-07-05 16:44:44 +0200

    update

--- resources/views/frontend/partials/category_menu.blade.php
+++ resources/views/frontend/partials/category_menu.blade.php
@@ -1,6 +1,6 @@
-<div class="aiz-category-menu bg-white rounded @if(Route::currentRouteName() == 'home') shadow-sm" @else shadow-lg" id="category-sidebar" @endif>
-    <div class="p-3 bg-soft-primary d-none d-lg-block rounded-top all-category position-relative text-left">
-        <span class="fw-600 fs-16 mr-3">{{ translate('Categories') }}</span>
+<div class="aiz-category-menu rounded style="background-color: #1D4C58;" @if(Route::currentRouteName() == 'home') shadow-sm" @else shadow-lg" id="category-sidebar" @endif>
+    <div class="p-3  d-none d-lg-block rounded-top all-category position-relative text-left">
+        <span class="fw-600 fs-16 mr-3" style="color: #FFFFFF;">{{ translate('Categories') }}</span>
         <a href="{{ route('categories.all') }}" class="text-reset">
             <span class="d-none d-lg-inline-block">{{ translate('See All') }} ></span>
         </a>
