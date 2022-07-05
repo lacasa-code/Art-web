36af9486aac7b494898e11858a77464158ea8f92
نورهان طاهر <norhan@lacasacode.come>
2022-07-05 16:14:33 +0200

    update

--- resources/views/frontend/partials/best_selling_section.blade.php
+++ resources/views/frontend/partials/best_selling_section.blade.php
@@ -12,7 +12,7 @@
                     <h3 class="h5 fw-700 mb-0">
                         <span class=" border-primary border-width-2 pb-3 d-inline-block" style="color: #FFFFFF;">{{ translate('Best Selling') }}</span>
                     </h3>
-                    <a href="javascript:void(0)" class="ml-auto mr-0 btn btn-primary btn-sm shadow-md">{{ translate('Top 20') }}</a>
+                    <a href="javascript:void(0)" class="ml-auto mr-0 btn btn-primary btn-sm shadow-md" style="color: #1D4C58;">{{ translate('Top 20') }}</a>
                 </div>
                 <div class="aiz-carousel gutters-10 half-outside-arrow" data-items="6" data-xl-items="5" data-lg-items="4"  data-md-items="3" data-sm-items="2" data-xs-items="2" data-arrows='true' data-infinite='true'>
                     @foreach ($best_selling_products as $key => $product)
