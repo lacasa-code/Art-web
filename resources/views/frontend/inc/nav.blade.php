c85c060d6a24c6f21f7da368aba69863d15b4b5b
نورهان طاهر <norhan@lacasacode.come>
2022-07-05 10:28:01 +0200

    update

--- resources/views/frontend/inc/nav.blade.php
+++ resources/views/frontend/inc/nav.blade.php
@@ -188,7 +188,7 @@
                         @if($header_logo != null)
                             <img src="{{ uploaded_asset($header_logo) }}" alt="{{ env('APP_NAME') }}" class="mw-100 h-30px h-md-40px" height="40">
                         @else
-                            <img src="{{ static_asset('assets/img/logo.png') }}" alt="{{ env('APP_NAME') }}" class="mw-100 h-30px h-md-40px" height="40">
+                            <img src="{{ static_asset('assets/img/dark.png') }}" alt="{{ env('APP_NAME') }}" class="mw-100 h-30px h-md-40px" height="40">
                         @endif
                     </a>
 
