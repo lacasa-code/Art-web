aeeb0112d7d04619cb64d6035cb65a96bbfbcd28
نورهان طاهر <norhan@lacasacode.come>
2022-07-06 11:16:23 +0200

    update

--- resources/views/frontend/all_brand.blade.php
+++ resources/views/frontend/all_brand.blade.php
@@ -5,12 +5,12 @@
 <section class="pt-4 mb-4">
     <div class="container">
         <div class="row">
-            <div class="col-lg-6 text-center text-lg-left">
+            <div class="col-lg-6 text-center text-lg-left" style="color: #FFFFFF;">
                 <h1 class="fw-600 h4">{{ translate('All Brands') }}</h1>
             </div>
             <div class="col-lg-6">
                 <ul class="breadcrumb bg-transparent p-0 justify-content-center justify-content-lg-end">
-                    <li class="breadcrumb-item opacity-50">
+                    <li class="breadcrumb-item opacity-50" style="color: #FFFFFF;">
                         <a class="text-reset" href="{{ route('home') }}">{{ translate('Home')}}</a>
                     </li>
                     <li class="text-dark fw-600 breadcrumb-item">
