d5c804fdea6fbb349b443cb7dc5e666d0f6b4a66
نورهان طاهر <norhan@lacasacode.come>
2022-07-06 11:19:38 +0200

    update

--- resources/views/frontend/all_category.blade.php
+++ resources/views/frontend/all_category.blade.php
@@ -4,15 +4,15 @@
 <section class="pt-4 mb-4">
     <div class="container text-center">
         <div class="row">
-            <div class="col-lg-6 text-center text-lg-left">
+            <div class="col-lg-6 text-center text-lg-left" style="color: #FFFFFF;">
                 <h1 class="fw-600 h4">{{ translate('All Categories') }}</h1>
             </div>
             <div class="col-lg-6">
                 <ul class="breadcrumb bg-transparent p-0 justify-content-center justify-content-lg-end">
-                    <li class="breadcrumb-item opacity-50">
+                    <li class="breadcrumb-item opacity-50" style="color: #FFFFFF;">
                         <a class="text-reset" href="{{ route('home') }}">{{ translate('Home')}}</a>
                     </li>
-                    <li class="text-dark fw-600 breadcrumb-item">
+                    <li class="text-dark fw-600 breadcrumb-item" >
                         <a class="text-reset" href="{{ route('categories.all') }}">"{{ translate('All Categories') }}"</a>
                     </li>
                 </ul>
@@ -24,7 +24,7 @@
     <div class="container">
         @foreach ($categories as $key => $category)
             <div class="mb-3 bg-white shadow-sm rounded">
-                <div class="p-3 border-bottom fs-16 fw-600">
+                <div class="p-3 border-bottom fs-16 fw-600" style="color: #FFFFFF;">
                     <a href="{{ route('products.category', $category->slug) }}" class="text-reset">{{  $category->getTranslation('name') }}</a>
                 </div>
                 <div class="p-3 p-lg-4">
