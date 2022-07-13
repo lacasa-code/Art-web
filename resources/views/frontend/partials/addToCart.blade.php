bbcf763738587f4d0ecd3f4a5fde09001435b8f5
نورهان طاهر <norhan@lacasacode.come>
2022-07-13 10:51:43 +0200

    update

--- resources/views/frontend/partials/addToCart.blade.php
+++ resources/views/frontend/partials/addToCart.blade.php
@@ -233,7 +233,7 @@
                         </div>
                         <div class="col-10">
                             <div class="product-price">
-                                <strong id="chosen_price" class="h4 fw-600 text-primary">
+                                <strong id="chosen_price" class="h4 fw-600 text-primary" style="color: #1D4C58;">
 
                                 </strong>
                             </div>
